5b55ae95515c0e1f0c652d0c53e6d24675978a36
Piotr Kucharek <piotrkucharek1@gmail.com>
2020-08-18 12:10:32 +0200

    Removed tests with negative numbers

--- test/calculator_tests.py
+++ test/calculator_tests.py
@@ -9,10 +9,8 @@ from main.calculator import add
     ("1,2", 3),
     ("2,1", 3),
     ("35,12", 47),
-    ("-35,0", -35),
-    ("-35,0,25,50,70,-2", 108),
-    ("-35,0,25,50,70,-2", 108),
-
+    ("35,0", 35),
+    ("35,0,25,50,70,2", 182),
 ])
 def test__when_given_numbers__returns_expected_value(numbers: str, expected: int) -> None:
     assert add(numbers) == expected
@@ -20,7 +18,7 @@ def test__when_given_numbers__returns_expected_value(numbers: str, expected: int
 
 @pytest.mark.parametrize("numbers,expected", [
     ("1\n0", 1),
-    ("-35\n0,25\n50,70\n-2", 108),
+    ("35\n0,25\n50,70\n2", 182),
     ("1\n2,3\n4", 10)
 ])
 def test__when_new_line_character__returns_expected_value(numbers: str, expected: int) -> None:
@@ -29,7 +27,7 @@ def test__when_new_line_character__returns_expected_value(numbers: str, expected
 
 @pytest.mark.parametrize("numbers,expected", [
     ("//;\n1;1", 2),
-    ("//#\n3#4#-2", 5),
+    ("//#\n3#4#2", 9),
     ("//abc\n45abc3abc8", 56)
 ])
 def test__when_declaring_different_delimiter__returns_expected_value(numbers: str, expected: int) -> None:
