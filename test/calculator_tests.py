c9b2ba09606f7e9c80fbf81d10d62f542c21b9e9
Piotr Kucharek <piotrkucharek1@gmail.com>
2020-08-18 09:12:09 +0200

    Refactored test cases to parametrized test

--- test/calculator_tests.py
+++ test/calculator_tests.py
@@ -1,17 +1,13 @@
+import pytest
+
 from main.calculator import add
 
 
-def test_when_empty_arguments_returns0():
-    assert add("") == 0
-
-
-def test__when_argument_is_one__returns1():
-    assert add("1") == 1
-
-
-def test__when_arguments_are_1_and_2__returns_3():
-    assert add("1,2") == 3
-
-
-def test__when_arguments_are_2_and_1__returns_3():
-    assert add("2,1") == 3
+@pytest.mark.parametrize("numbers,expected", [
+    ("", 0),
+    ("1", 1),
+    ("1,2", 3),
+    ("2,1", 3)
+])
+def test__when_given_numbers__returns_expected_value(numbers, expected):
+    assert add(numbers) == expected
