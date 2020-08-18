01ddcd8b02897836ebe6deafb3e560e7148a3a33
Piotr Kucharek <piotrkucharek1@gmail.com>
2020-08-18 09:33:15 +0200

    Handled multiple arguments

--- test/calculator_tests.py
+++ test/calculator_tests.py
@@ -10,6 +10,7 @@ from main.calculator import add
     ("2,1", 3),
     ("35,12", 47),
     ("-35,0", -35),
+    ("-35,0,25,50,70,-2", 108)
 ])
 def test__when_given_numbers__returns_expected_value(numbers: str, expected: int) -> None:
     assert add(numbers) == expected
