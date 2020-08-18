f50fdc8cb6e591711fd95d1573f617aaf68bbacf
Piotr Kucharek <piotrkucharek1@gmail.com>
2020-08-18 09:08:24 +0200

    Implemented 2 arguments

--- main/calculator.py
+++ main/calculator.py
@@ -1,4 +1,21 @@
-def add(numbers):
-    if numbers == "1":
-        return 1
-    return 0
+def add(arguments):
+    numbers = arguments.split(",")
+    if (
+        len(numbers) == 1
+        and numbers[0] == ""
+    ):
+        return 0
+    if (
+        len(numbers) == 1
+        and numbers[0] != ""
+    ):
+        return int(numbers[0])
+    if len(numbers) == 2:
+        return sum_numbers(numbers)
+    return -1
+
+
+def sum_numbers(numbers):
+    a = int(numbers[0])
+    b = int(numbers[1])
+    return a + b
--- test/calculator_tests.py
+++ test/calculator_tests.py
@@ -7,3 +7,11 @@ def test_when_empty_arguments_returns0():
 
 def test__when_argument_is_one__returns1():
     assert add("1") == 1
+
+
+def test__when_arguments_are_1_and_2__returns_3():
+    assert add("1,2") == 3
+
+
+def test__when_arguments_are_2_and_1__returns_3():
+    assert add("2,1") == 3
