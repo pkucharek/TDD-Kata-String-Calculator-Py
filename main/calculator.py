3fcad7e262f84ea38496987a8ead6e6b9cb2228b
Piotr Kucharek <piotrkucharek1@gmail.com>
2020-08-18 11:45:00 +0200

    Extracted method to prepare numbers for count

--- main/calculator.py
+++ main/calculator.py
@@ -3,18 +3,22 @@ from typing import List
 
 
 def add(arguments: str) -> int:
-    if arguments.startswith("//"):
-        separator: str = re.search("//(.+)\n", arguments).group(1)
-        arguments = arguments.replace(f"//{separator}\n", "")
-    else:
-        separator: str = "[\\n,]"
-    numbers: List[str] = re.split(separator, arguments)
+    numbers = prepare_numbers(arguments)
     if (
         len(numbers) == 1
         and numbers[0] == ""
     ):
         return 0
     return sum_numbers(numbers)
+
+
+def prepare_numbers(arguments: str) -> List[str]:
+    if arguments.startswith("//"):
+        separator: str = re.search("//(.+)\n", arguments).group(1)
+        arguments = arguments.replace(f"//{separator}\n", "")
+    else:
+        separator: str = "[\\n,]"
+    return re.split(separator, arguments)
 
 
 def sum_numbers(numbers: List[str]) -> int:
